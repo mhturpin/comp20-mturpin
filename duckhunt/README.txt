5ca9a13187250d94e03e90f99bf8418d31dd9733
Maxwell Turpin <maxwell.turpin@tufts.edu>
2015-10-07 17:02:56 -0400

    Wrote game.js to display image with 2 ducks and added README

---
+++ duckhunt/README.txt
@@ -0,0 +1,2 @@
+# duckhunt
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById("game_canvas");
+	var ctx = canvas.getContext("2d");
+	var bkground = new Image();
+	bkground.onload = function(){
+		ctx.drawImage(bkground, 0, 0, 800, 600);
+	};
+	bkground.src = 'duckhunt-background.gif';
+
+	var ducks = new Image();
+	ducks.src = 'duckhunt_various_sheet.png';
+	ducks.onload = function(){
+		ctx.drawImage(ducks, 35, 150, 40, 40, 330, 200, 100, 100);
+		ctx.drawImage(ducks, 210, 115, 40, 40, 600, 150, 100, 100);
+	};
+}
