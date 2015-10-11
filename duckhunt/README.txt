ab3b106a7116b370d9424cf101294e55e9a1d9b5
Maxwell Turpin <maxwell.turpin@tufts.edu>
2015-10-11 14:29:27 -0400

    Animated the ducks and wrote README.txt

--- duckhunt/README.txt
+++ duckhunt/README.txt
@@ -1,2 +1,6 @@
 # duckhunt
+1. Everything works properly. The images are displayed and the ducks flap their wings
+2. I referenced:
 https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+http://www.w3schools.com/jsref/met_win_setinterval.asp
+3. I spent about 2 hours on this
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,16 +1,36 @@
 function init() {
 	var canvas = document.getElementById("game_canvas");
 	var ctx = canvas.getContext("2d");
+
+	// load and draw background
 	var bkground = new Image();
 	bkground.onload = function(){
 		ctx.drawImage(bkground, 0, 0, 800, 600);
 	};
 	bkground.src = 'duckhunt-background.gif';
 
+	// load and animate ducks
 	var ducks = new Image();
 	ducks.src = 'duckhunt_various_sheet.png';
 	ducks.onload = function(){
-		ctx.drawImage(ducks, 35, 150, 40, 40, 330, 200, 100, 100);
-		ctx.drawImage(ducks, 210, 115, 40, 40, 600, 150, 100, 100);
+		flap(bkground,ducks, ctx);
 	};
+}
+
+function flap(back, ducks, ctx) {
+	blue_x = 0;
+	black_x = 129;
+	setInterval(function() {
+		// redraw background over old ducks
+		ctx.drawImage(back, 0, 0, 5, 5, 330, 200, 100, 100);
+		ctx.drawImage(back, 0, 0, 5, 5, 600, 150, 100, 100);
+
+		// draw ducks
+		ctx.drawImage(ducks, blue_x, 156, 38, 35, 330, 200, 100, 100);
+		ctx.drawImage(ducks, black_x, 117, 38, 35, 600, 150, 100, 100);
+
+		// set starting x position for both duck
+		blue_x = (blue_x+38)%114;
+		black_x = blue_x + 129;
+	}, 300);
 }
