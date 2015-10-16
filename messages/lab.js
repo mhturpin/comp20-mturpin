178c104e639e08d395a7674415e4cef811bc1de9
Maxwell Turpin <maxwell.turpin@tufts.edu>
2015-10-16 13:39:44 -0400

    Testing XMLHttpRequest

--- messages/lab.js
+++ messages/lab.js
@@ -1,9 +1,9 @@
 function parse() {
 	var request = new XMLHttpRequest();
 	request.open("POST", "data.json", true);
-	request.send();
 	request.onload = function() {
 		var messages = JSON.parse(request.responseText);
 		console.log(messages);
 	}
+	request.send();
 }
