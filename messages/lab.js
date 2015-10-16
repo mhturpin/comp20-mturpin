5bf8998366991f4d69886c5d7a4e2108e1b553c9
Maxwell Turpin <maxwell.turpin@tufts.edu>
2015-10-16 12:44:01 -0400

    Testing XMLHttpRequest

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,7 @@
-// Your JavaScript goes here...
+function parse() {
+	var request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	request.send();
+	var messages = JSON.parse(request.responseText);
+	console.log(messages);
+}
